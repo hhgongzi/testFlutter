8b93fb0abc4e73702c9b4fafa1f8572c3be7b834
zhaochao <zhaochao_personal@126.com>
2019-07-25 16:47:04 +0800

    container

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Welcome to Flutter',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Welcome to Flutter'),
-        ),
-        body: new Center(
-          child: new Text('Hello World'),
-        ),
-      ),
-    );
-  }
+void main() {
+  runApp(myApp);
 }
+
+var myApp1 = new Container(
+    child: new Text("Hellow,world12",
+        textDirection: TextDirection.ltr,
+        style: new TextStyle(fontSize: 28, fontWeight: FontWeight.w900)),
+    width: 100,
+    height: 100,
+    color: Colors.grey[300]);
+
+var myApp = new Container(
+    child: new Center(
+        child: new Container(
+            child: new Text("hellow",
+                textDirection: TextDirection.ltr,
+                style:
+                    new TextStyle(fontSize: 28, fontWeight: FontWeight.w900)),
+            padding: EdgeInsets.all(16.0),
+            decoration: new BoxDecoration(color: Colors.red[400]))),
+    width: 320.0,
+    height: 240.0,
+    color: Colors.grey[300]);
