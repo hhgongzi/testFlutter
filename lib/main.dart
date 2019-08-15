8846c4cdd95de452e70f2d201943e0d93042130d
zhaochao <zhaochao_personal@126.com>
2019-08-15 11:11:43 +0800

    tijiao

--- lib/main.dart
+++ lib/main.dart
@@ -6,35 +6,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-                title: Text("flutter"),
-            ),
-            body: HomePage(),
-        ),
+      home: Text("123"),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        SizedBox(
-          height: 20,
-        ),
-        Text("nihao"),
-        SizedBox(
-          height: 20,
-        ),
-        RaisedButton(
-          child: Text("按钮"),
-          onPressed: () {},
-        )
-      ],
-    );
-  }
-}
