4f4158eaed0c54e425f922094d78cb43830e6277
zhaochao <zhaochao_personal@126.com>
2019-08-29 11:01:49 +0800

    finish

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:date_format/date_format.dart';
+import 'package:flutter_localizations/flutter_localizations.dart'; //国际化
 
 void main() => runApp(MyApp());
 
@@ -9,6 +10,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('zh', 'CH'),
+        const Locale('en', 'US'),
+      ],
       home: Scaffold(
         appBar: AppBar(
           title: Text('flutter'),
@@ -27,6 +36,32 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   var now = new DateTime.now();
+  var _date = new DateTime.now();
+  var _time = TimeOfDay(hour: 12, minute: 20);
+  _showDatePicker() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(1980),
+      lastDate: DateTime(2100),
+    ).then((result) {
+      print(result);
+      setState(() {
+        _date = result;
+      });
+    });
+  }
+
+  _showTimePicker() async {
+    var result = await showTimePicker(
+      context: context,
+      initialTime: _time,
+    );
+    setState(() {
+      _time = result;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -36,7 +71,25 @@ class _HomePageState extends State<HomePage> {
         ),
         Container(
           child: Text("time:${formatDate(now, [yyyy, '-', mm, '-', dd])}"),
-        )
+        ),
+        InkWell(
+          child: Row(
+            children: <Widget>[
+              Text("${formatDate(_date, [yyyy, '-', mm, '-', dd])}"),
+              Icon(Icons.arrow_drop_down),
+            ],
+          ),
+          onTap: _showDatePicker,
+        ),
+        InkWell(
+          child: Row(
+            children: <Widget>[
+              Text("${_time.format(context)}"),
+              Icon(Icons.arrow_drop_down),
+            ],
+          ),
+          onTap: _showTimePicker,
+        ),
       ],
     );
   }
